a387ec551851b96dafe1aae7119ca8190bb7a9b5
Felipe Rosero <luisf@ideamosmas.com>
2020-05-31 14:03:27 -0500

    Servicios por idioma

--- src/app/admin-view/examples/statistics/statistics.component.ts
+++ src/app/admin-view/examples/statistics/statistics.component.ts
@@ -41,6 +41,17 @@ export class StatisticsComponent implements OnInit {
       backgroundColor: ['rgba(0,255,0,0.3)', 'rgba(255,0,0,0.3)',],
     },
   ];
+  languages: String[] = ['Automatic', 'Afrikaans', 'Albanian', 'Amharic', 'Arabic', 'Armenian', 'Azerbaijani', 'Basque',
+    'Belarusian', 'Bengali', 'Bosnian', 'Bulgarian', 'Catalan', 'Cebuano', 'Chichewa', 'Chinese (Simplified)',
+    'Chinese (Traditional)', 'Corsican', 'Croatian', 'Czech', 'Danish', 'Dutch', 'Ingles', 'Esperanto', 'Estonian', 'Filipino',
+    'Finnish', 'French', 'Frisian', 'Galician', 'Georgian', 'German', 'Greek', 'Gujarati', 'Haitian Creole', 'Hausa', 'Hawaiian',
+    'Hebrew', 'Hebrew', 'Hindi', 'Hmong', 'Hungarian', 'Icelandic', 'Igbo', 'Indonesian', 'Irish', 'Italian', 'Japanese',
+    'Javanese', 'Kannada', 'Kazakh', 'Khmer', 'Korean', 'Kurdish (Kurmanji)', 'Kyrgyz', 'Lao', 'Latin', 'Latvian', 'Lithuanian',
+    'Luxembourgish', 'Macedonian', 'Malagasy', 'Malay', 'Malayalam', 'Maltese', 'Maori', 'Marathi', 'Mongolian', 'Myanmar (Burmese)',
+    'Nepali', 'Norwegian', 'Pashto', 'Persian', 'Polish', 'Portuguese', 'Punjabi', 'Romanian', 'Russian', 'Samoan', 'Scots Gaelic',
+    'Serbian', 'Sesotho', 'Shona', 'Sindhi', 'Sinhala', 'Slovak', 'Slovenian', 'Somali', 'Spanish', 'Sundanese', 'Swahili',
+    'Swedish', 'Tajik', 'Tamil', 'Telugu', 'Thai', 'Turkish', 'Ukrainian', 'Urdu', 'Uzbek', 'Vietnamese', 'Welsh', 'Xhosa',
+    'Yiddish', 'Yoruba', 'Zulu'];
 
   constructor(public service: ServiceService) { }
 
@@ -56,32 +67,24 @@ export class StatisticsComponent implements OnInit {
     console.log(event, active);
   }
 
-  getEstadisticas(){
+  getEstadisticas() {
     this.pieChartLabels = [['Acertadas'], ['Fallidas']];
-    this.service.getEstadisticas().subscribe(data=>{
-      this.pieChartData = [data.acetadas, data.fallidas];
+    this.service.getEstadisticas().subscribe(data => {
+      this.pieChartData = [data[0].acertadas, data[0].fallidas];
       this.flagFull = true;
     })
   }
 
   getLanguageSelected(languageAux: string) {
     this.languange = languageAux;
-    this.flag = true;
     this.loadGraphicsByLanguage();
   }
 
   loadGraphicsByLanguage() {
-    switch (this.languange) {
-      case "Ingles":
-        this.pieChartData = [110, 250];
-        break;
-      case "Frances":
-        this.pieChartData = [288, 110];
-        break;
-      case "Italiano":
-        this.pieChartData = [174, 100];
-        break;
-    }
+    this.service.getEstadisticasIdioma(this.languange).subscribe(data=>{
+      this.pieChartData = [data[0].acertadas, data[0].fallidas];
+      this.flag = true;
+    })
   }
 
 
